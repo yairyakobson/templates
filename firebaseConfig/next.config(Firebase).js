12642fde1df1006c9754e813d4dee8ff1b44af8f
yairyakobson <yairnet92@gmail.com>
2025-01-21 12:09:38 +0200

    Firebase configuration for NextJS

---
+++ firebaseConfig/next.config(Firebase).js
@@ -0,0 +1,12 @@
+module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+        port: '',
+        pathname: '/v0/b/event-fetching.appspot.com/o/**',
+      },
+    ],
+  },
+}
