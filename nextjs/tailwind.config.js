5facad032b8209c1489989c7201c83b17e13fc35
yairyakobson <yairnet92@gmail.com>
2025-01-25 20:01:03 +0200

    TailwindCSS V3 Configuration

---
+++ nextjs/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    screens: {
+      sm: { max: "640px" },
+      md: { min: "641px", max: "768px" },
+      lg: { min: "769px", max: "1024px" },
+      xl: { min: "1025px", max: "1280px" },
+      "2xl": { min: "1281px", max: "1536px" },
+      "3xl": { min: "1537px", max: "1920px" }
+    }
+  },
+  plugins: [],
+};
