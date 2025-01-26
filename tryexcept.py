40847b61cb4a4a68c65501dd29150e38ec2aa550
yairyakobson <yairnet92@gmail.com>
2025-01-26 14:24:17 +0200

    Python try/except file

---
+++ tryexcept.py
@@ -0,0 +1,18 @@
+str = "Hey"
+
+try:
+    strconvert = int(str)
+except:
+    strconvert = -1
+
+print("First:", strconvert)
+
+str_two = "123"
+
+try:
+    strconvert_2 = int(str_two)
+except:
+    strconvert_2 = -1
+
+print("Second:", strconvert_2)
+
